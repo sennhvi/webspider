d372993dbfbd72f3c4347e86f2bab9274faca88f
sennhvi <sennhvi@gmail.com>
2018-06-29 23:49:29 +0800

    update

--- c3_urllib_demos/c3_urllib_request.py
+++ c3_urllib_demos/c3_urllib_request.py
@@ -1,8 +1,18 @@
+import urllib.parse
 import urllib.request
 
+# urlopen API:
+# urlopen(url, data=None, timeout=socket._GLOBAL_DEFAULT_TIMEOUT,
+#             *, cafile=None, capath=None, cadefault=False, context=None):
 response = urllib.request.urlopen('http://www.python.org')
 print(type(response))  # <class 'http.client.HTTPResponse'> object
 print(response.status)
 print(response.getheaders())
 print(response.getheader('Server'))
 print(response.read().decode('utf-8'))  # crawl source code of python homepage and print
+
+# args:
+# data: if its type is bytes stream, you will need bytes() method, then it becomes POST method
+data = bytes(urllib.parse.urlencode({'word':'hello'}), encoding='utf8')
+response = urllib.request.urlopen('http://httpbin.org/post', data=data)
+print(response.read())
