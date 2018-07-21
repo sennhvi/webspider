6fd22befc2a5d72b5fe37b3dc97d4d511aadd846
sennhvi <sennhvi@gmail.com>
2018-07-21 23:15:58 +0800

    update

---
+++ c3_urllib_demos/c3_urllib_request_Handler.py
@@ -0,0 +1,44 @@
+# HTTPDefaultErrorHandler
+# HTTPRedirectHandler
+# HTTPCookieProcessor
+# ProxyHandler
+# HTTPPasswordMgr
+# HTTPBasicAuthHandler
+
+
+# Authorization
+from urllib.request import HTTPPasswordMgrWithDefaultRealm, HTTPBasicAuthHandler, build_opener
+from urllib.error import URLError
+# Proxy
+from urllib.request import ProxyHandler, build_opener
+
+"""Authorization Sample"""
+username = 'username'
+password = 'password'
+url = 'http://localhost:5000'
+
+p = HTTPPasswordMgrWithDefaultRealm()
+p.add_password(None, url, username, password)
+auth_handler = HTTPBasicAuthHandler(p)
+opener = build_opener(auth_handler)
+
+try:
+    result = opener.open(url)
+    html = result.read().decode('utf-8')
+    print(html)
+except URLError as e:
+    print(e.reason)
+
+"""ProxyHandler Sample"""
+proxy_handler = ProxyHandler(
+    {
+        'http': 'http://127.0.0.1:9743',
+        'https': 'https://127.0.0.1:9743'
+    }
+)
+opener = build_opener(proxy_handler)
+try:
+    response = opener.open('https://www.baidu.com')
+    print(response.read().decode('utf-8'))
+except URLError as e:
+    print(e.reason)
