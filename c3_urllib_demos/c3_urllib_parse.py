6ab14301aed1c31d71d7df95e0d7a2956922aceb
sennhvi <sennhvi@gmail.com>
2018-07-26 23:27:38 +0800

    add c3 urllib parse module

--- c3_urllib_demos/c3_urllib_parse.py
+++ c3_urllib_demos/c3_urllib_parse.py
@@ -1,6 +1,7 @@
-from urllib.parse import urlparse, urlunparse, urlsplit, urlunsplit
+from urllib.parse import urlparse, urlunparse, urlsplit, urlunsplit, urljoin, urlencode, parse_qs, parse_qsl, quote, \
+    unquote
 
-"""urlparse()"""
+"""1. urlparse()"""
 # urllib.parse.urlparse(urlstring, scheme='', allow_fragments=True)
 # scheme://netloc/path;params?query#fragment
 # scheme only works when no scheme specified in URL
@@ -8,27 +9,59 @@ from urllib.parse import urlparse, urlunparse, urlsplit, urlunsplit
 result = urlparse('https://www.baidu.com/index.html;user?id=5#comment')
 print(type(result), result)
 
-
-"""urlunparse()"""
+"""2. urlunparse()"""
 # construct url
 # argument length must be 6
 data = ['http', 'www.baidu.com', 'index.html', 'user', 'a=6', 'comment']
 print(urlunparse(data))
 
-
-"""urlsplit()"""
+"""3. urlsplit()"""
 # return tuple/SplitResult object
 result = urlsplit('http://www.baidu.com/index.html;user?id=5#comment')
 print(result, result.scheme, result[0], sep='\n')
 
-
-"""urlunsplit()"""
+"""4. urlunsplit()"""
 # construct url
 # argument length must be 5
 data = ['http', 'www.baidu.com', 'index.html', 'a=6', 'comment']
 print(urlunsplit(data))
 
-
-"""urljoin()"""
+"""5. urljoin()"""
 # construct url using base_url and target_url
 # base_url only provides: scheme, netloc, path as reference
+print(urljoin('http://www.baidu.com', 'FAQ.html'))
+print(urljoin('http://www.baidu.com/about.html', 'https://sb.com/FAQ.html'))
+print(urljoin('http://www.baidu.com?wd=abc', 'https://sb.com/FAQ.html?question=2'))
+print(urljoin('http://www.baidu.com', '?category=2#comment'))
+print(urljoin('http://www.baidu.com#comment', '?category=2'))
+
+"""6. urlencode()"""
+# construct request args of GET method
+params = {
+    'name': 'germey',
+    'age': 22
+}
+base_url = 'http://www.baidu.com?'
+url = base_url + urlencode(params)
+print(url)
+
+"""7. parse_qs()"""
+# deserialization
+query = 'name=germey&age=22'
+print(parse_qs(query))
+
+"""8. parse_qsl()"""
+# convert to tuple list
+query = 'name=germey&age=22'
+print(parse_qsl(query))
+
+"""9. quote()"""
+# convert content to URL encoded format
+keyword = '壁纸'
+url = 'https://www.baidu.com/s?wd=' + quote(keyword)
+print(url)
+
+"""10. unquote()"""
+# decode URL
+url = "https://www.baidu.com/s?wd=%E5%A3%81%E7%BA%B8"
+print(unquote(url))
