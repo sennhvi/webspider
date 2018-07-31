4bee4badbf1b17a5e72931d66f6f264cdae0e616
sennhvi <sennhvi@gmail.com>
2018-08-01 00:39:19 +0800

    update

--- c3_urllib_demos/c3_requests.py
+++ c3_urllib_demos/c3_requests.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 r = requests.get('https://www.baidu.com')
 print(type(r))
@@ -17,3 +18,19 @@ r = requests.get('http://httpbin.org/get', params=data)
 print(r.text)
 print(r.json())  # output type is str type in json format, use json() method to convert str to dict
 print(type(r.json()))
+
+"""crawl source data of website"""
+# re.S = DOTALL = sre_compile.SRE_FLAG_DOTALL # make dot match newline
+headers = {
+    'User-Agent': "Mozilla/5.0 (X11; Linux x86_64)"
+                  " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36"
+}
+r = requests.get('https://www.zhihu.com/explore', headers=headers)
+pattern = re.compile('explore-feed.*?question_link.*?>(.*?)</a>', re.S)
+titles = re.findall(pattern, r.text)
+print(titles)
+
+"""crawl binary data and save to file"""
+r = requests.get('https://github.com/favicon.ico')
+with open('favicon.ico', 'wb') as f:
+    f.write(r.content)
